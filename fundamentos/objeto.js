7adc51308caee13a6d12cdbd9ef2f4e44ce4ada2
Daniel Nunes <danielmnunes08@gmail.com>
2020-06-14 17:53:54 -0300

    objetos

---
+++ fundamentos/objeto.js
@@ -0,0 +1,12 @@
+// Objeto pode ser criado com {}
+let objeto = {}
+
+// Definir atributos dinamicamente
+objeto.nome = 'Daniel Nunes'
+objeto.idade = 24
+objeto.cpf = '123.456.789-10'
+// Objeto é uma estrutura do tipo Chave-Valor, semelhante 
+
+console.log(objeto)
+
+console.log(objeto.keys)
