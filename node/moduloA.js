4afd61bfeeb836dccb4a00c4e9d91c638f341aa7
Daniel Nunes <danielmnunes08@gmail.com>
2020-06-20 17:24:52 -0300

    Node

---
+++ node/moduloA.js
@@ -0,0 +1,4 @@
+this.ola = 'Olá Mundo!!'
+exports.bemVindo = 'Bem vindo ao Node!!'
+module.exports.ateLogo = 'Até logo!!'
+
---
+++ node/moduloCliente.js
@@ -0,0 +1,5 @@
+const moduloA = require('./moduloA')
+const moduloB = require('./moduloB')
+
+console.log(typeof moduloA)
+console.log(moduloA)
